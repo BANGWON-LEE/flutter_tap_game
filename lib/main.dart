513fb98044ce7656fb71fce02419464163dc5c51
BANGWON-LEE <qkddnjs31@gmail.com>
2023-01-23 17:33:09 +0900

    20230123 git first update

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:practice/screen/about_state.dart';
+import 'package:practice/screen/album.dart';
+import 'package:practice/screen/game/lobby.dart';
+import 'package:practice/screen/game/login.dart';
+import 'package:practice/screen/game/register.dart';
+import 'package:practice/screen/my_ondo.dart';
+import 'package:practice/screen/game/tap_game.dart';
+import 'package:practice/screen/test.dart';
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+
+void main() async {
+  await dotenv.load(fileName: 'asset/config/.env');
+  runApp(MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Login(),
+        '/register': (context) => Register(),
+        '/lobby': (context) => Lobby(),
+        '/game' : (context) => TapGame(),
+      }
+  )
+  );
+}
+
---
+++ lib/screen/game/lobby.dart
@@ -0,0 +1,369 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class Lobby extends StatefulWidget {
+  const Lobby({Key? key}) : super(key: key);
+
+  @override
+  State<Lobby> createState() => _LobbyState();
+}
+
+
+
+class _LobbyState extends State<Lobby> {
+
+  var myRecord;
+
+  // void initRecord() async {
+  //   final prefs = await SharedPreferences.getInstance();
+  //   var rec = prefs.getInt('rc_record');
+  //
+  //
+  // }
+  var recentRecord;
+
+  void initialRecentRecord() async{
+    final prefs = await SharedPreferences.getInstance();
+    prefs.remove('recent_rc_record');
+  }
+
+  String? baseUrl = dotenv.env['BASE_URL'];
+  void getUserRecordHttp() async {
+    final prefs = await SharedPreferences.getInstance();
+
+
+    var mem_userId = prefs.getString('userId');
+
+    BaseOptions options = BaseOptions(
+      baseUrl: baseUrl!,
+      connectTimeout: 3000,
+      receiveTimeout: 3000,
+    );
+
+    var recntRcRecord = prefs.getInt('recent_rc_record');
+    print('recent Record1 ${recntRcRecord}');
+    recentRecord = recntRcRecord == null ? null : '${recntRcRecord! ~/100}.${'${recntRcRecord! % 100}'.padLeft(2, '0')}';
+    print('recent Record2 ${recentRecord}');
+
+    Dio dio = new Dio(options);
+  try {
+    final res = await dio.post(
+        "/api/user/record", data: {"mem_userId": mem_userId});
+    print('response ${res.data[0]}');
+    var userArray = res.data[0];
+    if(res == []){
+      print('아직 입니다.');
+    } else {
+      print('저장합니다.');
+      prefs.setInt('rc_record', userArray['rc_record']);
+      var sec = userArray['rc_record']! ~/ 100;
+      var hundredth = '${userArray['rc_record'] % 100}'.padLeft(2, '0');
+      myRecord = '${sec}.${hundredth}';
+
+    }
+  } catch(e){
+    await dio.post("/api/register/level1", data: {"mem_userId": mem_userId});
+}
+  }
+
+  var rcData;
+
+
+  void getRecordHttp() async {
+    print('start game');
+    BaseOptions options = BaseOptions(
+      baseUrl: baseUrl!,
+      connectTimeout: 3000,
+      receiveTimeout: 3000,
+    );
+    Dio dio = new Dio(options);
+
+      try {
+
+        final res2 = await dio.get("/api/record/level1/total");
+
+        if(res2 != []) {
+          rcData = res2.data;
+        }
+        // print('record total rank ${rcData[1].values.toList()[0]}');
+
+      } catch (e) {
+        // _showMyDialog();
+      }
+  }
+
+  var myRank;
+
+  void getMyRank() async{
+
+    final prefs = await SharedPreferences.getInstance();
+
+
+    BaseOptions options = BaseOptions(
+      baseUrl: baseUrl!,
+      // baseUrl: '192.168.0.206:5000',
+      connectTimeout: 3000,
+      receiveTimeout: 3000,
+    );
+
+    Dio dio = new Dio(options);
+
+    try {
+
+      final res = await dio.post("/api/record/level1/my/rank",  data: {"mem_id": prefs.getInt('memId')} );
+
+      if(res != []) {
+        myRank = res.data[0];
+      }
+      print('myRank ${myRank}');
+    } catch (e) {
+      // _showMyDialog();
+    }
+  }
+
+
+
+  @override
+  void dispose() {
+    myRecord = 0;
+    print('dispose lobby');
+    initialRecentRecord();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // initRecord();
+    getMyRank();
+    getUserRecordHttp();
+    getRecordHttp();
+
+  }
+
+
+
+  // void setRegisterRecordHttp() async {
+  //   BaseOptions options = BaseOptions(
+  //     baseUrl: 'http://192.168.123.110:5000',
+  //     connectTimeout: 3000,
+  //     receiveTimeout: 3000,
+  //   );
+  //   Dio dio = new Dio(options);
+  //
+  //   try{
+  //     final res = await dio.post("/api/register/level1", data: {"mem_userId": idController.text });
+  //     print('response ${res}');
+  //     if(res.statusCode == 200){
+  //       Navigator.pushNamed(context, '/');
+  //     }
+  //   }catch(e){
+  //     // _showMyDialog();
+  //   }
+  // }
+
+  List<int> rankNum = [0,1,2];
+
+
+  final Future<String> _calculation = Future<String>.delayed(
+    const Duration(seconds: 1),
+        () => 'Data Loaded',
+  );
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: Text(
+                '랭킹 Top3',
+                style: TextStyle(
+                  fontSize: 55.0
+                ),
+              ),
+            ),
+            Container(
+              child:
+                Column(
+                  // mainAxisAlignment: MainAxisAlignment.center,
+                  // crossAxisAlignment: CrossAxisAlignment.center,
+                  children:
+                  rankNum.map((el) =>
+                    Container(
+                      child:
+                        Padding(
+                          padding: const EdgeInsets.only(top:20.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+
+                             children: [
+                              Padding(
+                                padding: const EdgeInsets.only(right: 1.0),
+                                child:  Image.asset('asset/img/crown_${el}.png',
+                                  width: 30,
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 15),
+                                child: FutureBuilder<String>(
+                                  future: _calculation, // a previously-obtained Future<String> or null
+                                  builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                                    List<Widget> children;
+                                    if (snapshot.hasData) {
+                                      children = <Widget>[
+                                    Text('${rcData[el].values
+                                        .toList()[1]} : ${rcData[el].values
+                                        .toList()[0] ~/ 100}.${'${ rcData[el]
+                                        .values.toList()[0] % 100}'.padLeft(
+                                    2, '0')} 초',
+                                    style: TextStyle(
+                                    fontSize: 20.0
+                                    ),
+                                    ),
+                                      ];
+                                    } else if (snapshot.hasError) {
+                                      children = <Widget>[
+                                        const Icon(
+                                          Icons.error_outline,
+                                          color: Colors.red,
+                                          size: 60,
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.only(top: 16),
+                                          child: Text('Error: ${snapshot.error}'),
+                                        ),
+                                      ];
+                                    } else {
+                                      children = const <Widget>[
+                                        SizedBox(
+                                          width: 15,
+                                          height: 15,
+                                          child: CircularProgressIndicator(),
+                                        ),
+                                        // Padding(
+                                        //   padding: EdgeInsets.only(top: 16),
+                                        //   child: Text('랭킹을 불러오고 있습니다.'),
+                                        // ),
+                                      ];
+                                    }
+                                    return Center(
+                                      child: Column(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: children,
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                    ),
+                  ).toList(),
+                ),
+              ),
+            Padding(
+              padding: const EdgeInsets.only(top:25.0),
+              child: Container(
+                child: FutureBuilder<String>(
+                  future: _calculation, // a previously-obtained Future<String> or null
+                  builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                    List<Widget> children;
+                    if (snapshot.hasData) {
+                      children = <Widget>[
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 15.0),
+                          child: Text(
+                          '나의 최고기록 ${myRecord!} 초'
+                            ,
+                          style: TextStyle(
+                          fontSize: 25.0
+                          ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 15.0),
+                          child: Text(
+                            '현재 기록 ${recentRecord == null ? 0.00 : recentRecord!} 초'
+                            ,
+                            style: TextStyle(
+                                fontSize: 25.0
+                            ),
+                          ),
+                        ),
+                        Text(
+                          '현재 순위 ${myRank['count(*)']} 위'
+                          ,
+                          style: TextStyle(
+                              fontSize: 25.0
+                          ),
+                        ),
+                      ],
+                    ),
+                      ];
+                    } else if (snapshot.hasError) {
+                      children = <Widget>[
+                        const Icon(
+                          Icons.error_outline,
+                          color: Colors.red,
+                          size: 60,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 16),
+                          child: Text('Error: ${snapshot.error}'),
+                        ),
+                      ];
+                    } else {
+                      children = const <Widget>[
+                        SizedBox(
+                          width: 15,
+                          height: 15,
+                          child: CircularProgressIndicator(),
+                        ),
+                        // Padding(
+                        //   padding: EdgeInsets.only(top: 16),
+                        //   child: Text('기록을 불러오고 있습니다.'),
+                        // ),
+                      ];
+                    }
+                    return Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: children,
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 55.0),
+              child: Container(
+                width: 130,
+                child: ElevatedButton(
+                    onPressed: (){
+                      Navigator.pushNamed(context, '/game');
+                    },
+                    child: Text(
+                      '게임입장',
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    )
+                ),
+              ),
+            )
+            ],
+          ),
+        ),
+      );
+    }
+  }
+
